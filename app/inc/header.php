8ef4d95ccee23fe8ef63acdff7a1253c8c962b33
Marek Pástor <marekpastorml@gmail.com>
2024-02-15 20:16:20 +0100

    Úpravy v headeri

--- app/inc/header.php
+++ app/inc/header.php
@@ -1,3 +1,4 @@
+<?php include 'config/common.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +6,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <title>QR Code app</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <title>Zdieľanie platby</title>
 </head>
 <body>
   <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
@@ -17,17 +19,26 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <a class="nav-link" href="/qrcode-app/app/index.php">Domov</a>
+            <a class="nav-link" href="/qrcode-app/app/index.php">IBAN</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/qrcode-app/app/qrcode.php">Generovanie QR kódu</a>
+            <a class="nav-link" href="/qrcode-app/app/qrcode.php">Zdieľanie platby</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/qrcode-app/app/about.php">O apke</a>
           </li>
-          <li class="nav-item" style="background-color: #cccccc; border-radius: 5px;">
+          <?php 
+          if (isset($_SESSION['user_id'])){
+            // Používateľ je prihlásený, nezobrazujeme prihlasovacie element
+            echo ('<li class="nav-item" style="background-color: #cccccc; border-radius: 5px;">
+            <a class="nav-link" href="/qrcode-app/app/login.php" style="font-weight: bold; color: #000;">logged in as '. $_SESSION['user_id'] .'</a>
+          </li>');
+          }else{
+            echo ('<li class="nav-item" style="background-color: #cccccc; border-radius: 5px;">
             <a class="nav-link" href="/qrcode-app/app/login.php" style="font-weight: bold; color: #000;">QR Code Login</a>
-          </li>
+          </li>');
+          }
+          ?>
         </ul>
       </div>
   </div>
