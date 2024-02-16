08279fe66079f59c207e95b9dec23aa18ce2db3a
Marek Pástor <marekpastorml@gmail.com>
2024-02-16 20:48:58 +0100

    Vytvorenie IBAN-u + funkcionalita

---
+++ app/config/add_iban_process.php
@@ -0,0 +1,71 @@
+<?php
+
+include 'database.php';
+include 'common.php';
+
+// Kontrola, či už je používateľ prihlásený
+if (!isset($_SESSION['user_id'])) {
+    $_SESSION['error4'] = "Nie ste prihlásený/á. Prihláste sa a skúste to znovu.";
+    header("Location: /qrcode-app/app/index.php");
+    exit();
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Spracovanie nového IBAN-u
+    $user_id = $_SESSION['user_id'];
+    $iban = $_POST['iban'];
+
+    // Kontrola, IBAN začíná prefixom "SK"
+    if (substr($iban, 0, 2) !== 'SK') {
+        $_SESSION['error5'] = "IBAN musí začínať prefixom 'SK'.";
+        header("Location: /qrcode-app/app/index.php");
+        exit();
+    }
+
+    // Za prefixom "SK" len číslice
+    if (!preg_match('/^SK\d+$/', substr($iban, 0, 4))) {
+        $_SESSION['error6'] = "IBAN po prefixe 'SK' môže obsahovať len číslice.";
+        header("Location: /qrcode-app/app/index.php");
+        exit();
+    }
+
+    // Kontrola, či IBAN má správny formát (prefix "SK" a 22 čísiel)
+    if (!preg_match('/^SK\d{22}$/', $iban)) {
+        $_SESSION['error7'] = "IBAN musí mať správny formát.";
+        header("Location: /qrcode-app/app/index.php");
+        exit();
+    }
+
+    // Kontrola či už IBAN neexistuje v databáze
+    $checkIBAN = "SELECT id FROM iban WHERE iban = ?";
+    $checkIBANsmt = $conn-> prepare($checkIBAN);
+    $checkIBANsmt-> bind_param("s", $iban);
+    $checkIBANsmt-> execute();
+    $checkIBANresult = $checkIBANsmt->get_result();
+
+    if ($checkIBANresult-> num_rows > 0){
+        $_SESSION['error8'] = "Nemôžete si pridať už raz pridaný IBAN, použite iný.";
+        header("Location: /qrcode-app/app/index.php");
+        exit();
+    }
+
+    // Vloženie nového IBAN do databázy
+    $sql = "INSERT INTO iban (iban_id, iban) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("is", $user_id, $iban);
+
+    if ($stmt->execute()) {
+        $_SESSION['success3'] = "IBAN bol úspešne pridaný.";
+    } else {
+        //$_SESSION['error7'] = "Chyba pri pridávaní IBAN-u: ";
+        header("Location: /qrcode-app/app/index.php");
+    }
+
+    $stmt->close();
+}
+
+// Presmerovanie na hlavnú stránku
+header("Location: /qrcode-app/app/index.php");
+exit();
+?>
---
+++ app/payment_sharing.php
@@ -0,0 +1,146 @@
+<?php 
+    include 'inc/header.php';
+    include 'config/database.php';
+
+
+    $iban = $sum = "";
+    $ibanErr = $sumErr = "";
+    /*
+        $date_iban = $email = $name = $info_name = $adress = $vs = $ss = $ks = $moneytype = "";
+        $date_ibanErr = $emailErr = $nameErr = $info_nameErr = $adressErr $vsErr = $ssErr = $ksErr = $moneytypeErr = "";
+    */
+    // Form submit
+    if (isset($_POST['submit'])){
+        // Validate IBAN
+        if (empty($_POST['iban'])){
+            $ibanErr = 'IBAN je potrebný';
+        }else{
+            $iban = filter_input(INPUT_POST, 'iban', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        }
+    
+        // Validate sum
+        if (empty($_POST['sum'])){
+            $sumErr = 'Suma je potrebná';
+        }else{
+            $sum = filter_input(INPUT_POST, 'sum', FILTER_SANITIZE_NUMBER_INT);
+        }
+    
+        if (empty($ibanErr) && empty($sumErr)){
+            // Add to database
+            $sql = "INSERT INTO qrcode (email, iban, sum, vs, ss, ks, moneytype, name, info_name, adress, date_iban) VALUES ('$email', '$iban', '$sum', '$vs', '$ss', '$ks', '$moneytype', '$name', '$info_name', '$adress', '$date_iban')";
+            if (mysqli_query($conn, $sql)){
+                // Succes
+                header('Location: index.php');
+            }else {
+                // Error
+                echo 'Error: ' . mysqli_error($conn);
+            }
+        }
+    }
+    // Zatiaľ iba takto pre overenie.
+    // Kontrola, či je používateľ prihlásený
+    if (!isset($_SESSION['user_id'])) {
+        echo "<p>Pre zobrazenie vašich IBAN-ov sa prosím prihláste.</p>";
+    } else {
+        // Získanie IBAN-ov pre prihláseného užívateľa
+        $user_id = $_SESSION['user_id'];
+        $getIBAN = "SELECT iban FROM iban WHERE iban_id = ?";
+        $getIBANstmt = $conn->prepare($getIBAN);
+        $getIBANstmt->bind_param("i", $user_id);
+        $getIBANstmt->execute();
+        $result = $getIBANstmt->get_result();
+
+        if ($result->num_rows > 0) {
+            echo "<h3>Pridané IBAN-y pre prihláseného užívateľa:</h3>";
+            echo "<ul>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<li>" . htmlspecialchars($row['iban']) . "</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo "<p>Žiadne pridané IBAN-y pre prihláseného užívateľa.</p>";
+        }
+
+        $getIBANstmt->close();
+    }
+?>
+
+<!-- Form for all inputs (Túto časť kódu mám ešte rozpracovanú.-->
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="mt-4 w-75">
+  <div class="row">
+    <div class="col-md-6">
+        <div class="mb-3">
+            <label for="iban" class="form-label">IBAN</label>
+            <input type="text" class="form-control <?php echo !$ibanErr ?: 'is-invalid';?>" id="iban" name="iban" placeholder="SK88 8888 8888 8888 8888 8888">
+            <div class="invalid-feedback">
+            <?php echo $ibanErr; ?>
+            </div>
+      </div>
+      <!--
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control < ?php echo !$emailErr ?: 'is-invalid';?>" id="email" name="email" placeholder="Zadajte email">
+        <div class="invalid-feedback">
+          < ?php echo $emailErr; ?>
+        </div>
+      </div>
+      -->
+      <div class="mb-3">
+        <label for="moneytype" class="form-label">Mena prevodu</label>
+        <input type="text" class="form-control" id="moneytype" name="moneytype" maxlength="3" placeholder="EUR">
+      </div>
+
+      <div class="mb-3">
+        <label for="ks" class="form-label">Konštantný symbol</label>
+        <input type="text" class="form-control" id="ks" name="ks" placeholder="1234">
+      </div>
+    </div>
+
+    <div class="col-md-6">
+        <div class="mb-3">
+        <label for="sum" class="form-label">Suma</label>
+        <input type="number" class="form-control <?php echo !$sumErr ?: 'is-invalid';?>" id="sum" name="sum" placeholder="10.00">
+        <div class="invalid-feedback">
+          <?php echo $sumErr; ?>
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <label for="vs" class="form-label">Variabilný symbol</label>
+        <input type="text" class="form-control" id="vs" name="vs" placeholder="9876543210">
+      </div>
+
+      <div class="mb-3">
+        <label for="ss" class="form-label">Špecifický symbol</label>
+        <input type="text" class="form-control" id="ss" name="ss" placeholder="1234567890">
+      </div>
+
+      <div class="mb-3">
+        <label for="date_iban" class="form-label">Splatnosť platobného príkazu</label>
+        <input type="date" class="form-control" id="date_iban" name="date_iban" placeholder="dd. mm. rrrr">
+      </div>
+    </div>
+
+    <div class="mb-3">
+        <label for="info_name" class="form-label">Informácia pre príjemcu</label>
+        <input type="text" class="form-control <" id="info_name" name="info_name" placeholder="Informácia pre príjemcu">
+    </div>
+
+    <div class="mb-3">
+        <label for="name" class="form-label">Názov príjemcu</label>
+        <input type="text" class="form-control <" id="name" name="name" placeholder="Názov príjemcu">
+    </div>
+
+    <div class="mb-3">
+        <label for="adress" class="form-label">Adresa 1. riadok</label>
+        <input type="text" class="form-control <" id="adress" name="adress" placeholder="Adresa 1. riadok">
+    </div>
+  </div>
+  
+  <div class="mb-3">
+    <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
+  </div>
+</form>
+
+
+<?php include 'inc/footer.php'; ?>
