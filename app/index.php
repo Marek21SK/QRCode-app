945d65bcfbb915be85b6ff91e918b81aad712328
Marek Pástor <marekpastorml@gmail.com>
2024-02-18 12:35:22 +0100

    Pridanie skriptu pre lepšie zobrazenie IBAN-u

--- app/index.php
+++ app/index.php
@@ -46,14 +46,22 @@ if (isset($_SESSION['user_id'])) {
       <div class="row">
         <div class="mb-3" style="text-align: center;">
           <label style="text-align: center;" for="iban" class="form-label">Zadajte IBAN:</label>
-          <input style="padding-right: 25px; margin: 10px auto;" type="text" class="form-control" id="iban" name="iban" placeholder="SK88 8888 8888 8888 8888 8888" maxlength="24" required>
+          <input style="padding-right: 25px; margin: 10px auto;" type="text" class="form-control" id="iban" name="iban" placeholder="SK88 8888 8888 8888 8888 8888" maxlength="29" required>
+          <!-- Skript, ktorý pri pridávaní IBAN-u sa zobrazí tak, že každé 4 znaky dá medzeru pre lepšie skontrolovanie -->
+          <script>
+            document.getElementById('iban').addEventListener('input', function(event){
+              var ibanSpaces = event.target.value.replace(/\s/g, '');
+              var ibanFormat = ibanSpaces.replace(/(.{4})/g, '$1 ').trim();
+              event.target.value = ibanFormat;
+            });
+          </script>
           <div class="d-grid">
             <?php if (isset($_SESSION['error4'])) { 
               echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error4'] . '</div>'; unset($_SESSION['error4']);}
               if (isset($_SESSION['error5'])) { 
                 echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error5'] . '</div>'; unset($_SESSION['error5']);}
               if (isset($_SESSION['success3'])) { 
-                echo '<div class="alert alert-sucess" role="alert">' . $_SESSION['success3'] . '</div>'; unset($_SESSION['success3']);}
+                echo '<div class="alert alert-info" role="alert">' . $_SESSION['success3'] . '</div>'; unset($_SESSION['success3']);}
               if (isset($_SESSION['error6'])) { 
                 echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error6'] . '</div>'; unset($_SESSION['error6']);}
               if (isset($_SESSION['error7'])) { 
