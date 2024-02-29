5b97a062e6c357d152f759d39ea8a4a81f84d56d
Marek Pástor <marekpastorml@gmail.com>
2024-02-29 11:01:41 +0100

    Implementovanie API + opravy chýb

--- app/payment.php
+++ app/payment.php
@@ -205,7 +205,7 @@
   
   <div class="mb-3 d-flex justify-content-between">
   <?php if ($loggedUser): ?>
-    <input type="submit" name="submit" value="Odoslať" class="btn btn-primary ms-auto mx-2" style="width: 450px;">
+    <input type="submit" name="submit" value="Uložiť platbu" class="btn btn-primary ms-auto mx-2" style="width: 450px;">
     <button type="button" class="btn btn-secondary me-auto mx-2" data-bs-toggle="modal" data-bs-target="#previewModal" style="width: 450px;">Ukážka</button>
     <?php else: ?>
       <h5 class="text-center mx-auto">Pre prácu s týmto formulárom musíte byť prihlásený</h5>
@@ -226,7 +226,7 @@
             <div class="col-md-6">
               <div class="mb-3">
                 <label for="prewiev_payment_id" class="form-label">IBAN</label>
-                  <select class="form-control" id="prewiev_payment_id" name="prewiev_payment_id" disabled>
+                  <select class="form-control" id="preview_payment_id" name="preview_payment_id" disabled>
                   <option value="" selected disabled>Vyberte IBAN</option>
                   <?php foreach ($savedIBANs as $ibanOption): ?>
                     <option value="<?php echo $ibanOption; ?>" <?php echo (isset($_POST['payment_id']) && $_POST['payment_id'] == $ibanOption) ? 'selected' : ''; ?>>
@@ -288,13 +288,102 @@
                 <label for="preview_adress2" class="form-label">Adresa 2. riadok</label>
                 <input type="text" class="form-control <" id="preview_adress2" name="preview_adress2" value="<?php echo isset($_POST['adress2']) ? htmlspecialchars($_POST['adress2']) : ''; ?>" disabled>
             </div>
+            <div id="qrPrompt" class="text-center mx-auto mb-3"><h5>Tu sa zobrazí vygenerovaný QR kód</h5></div>
+            <div id="qrPrompt2" class="text-center mx-auto mb-3"><h5>Váš vygenerovaný QR kód</h5></div>
+            <?php echo '<style> #qrPrompt2 {display: none;} </style>';?>
           </div>
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zavrieť</button>
+            <button type="button" class="btn btn-primary" onclick="generateQRCode()">Vytvoriť QR kód</button>
         </div>
     </div>
 </div>
 </div>
+
+<!-- IMPLEMENTOVANIE API -->
+<?php
+$date = "2024-03-10 00:00:00"; // Formát našich dátumov v databáze je 'YYYY-MM-DD HH:MM:SS' 
+$paymentDueDate = date('Ymd', strtotime($date)); // preformátovanie na 'YYYYMMDD' tak ako to API vyžaduje
+?>
+
+<!-- Generovanie QR kódov funguje iba na skutočných IBAN číslach -->
+<script>
+  function generateQRCode() {
+    // Získanie hodnôt z príslušných polí vo formulári
+    var iban = $('#preview_payment_id').val();
+    var moneytype = $('#preview_moneytype').val();
+    var ks = $('#preview_ks').val();
+    var sum = $('#preview_sum').val();
+    var vs = $('#preview_vs').val();	
+    var ss = $('#preview_ss').val();
+    var date_iban = $('#preview_date_iban').val();
+    var info_name = $('#preview_info_name').val();
+    var name = $('#preview_name').val();
+    var adress = $('#preview_adress').val();
+    var adress2 = $('#preview_adress2').val();
+
+    if (!iban || !sum || iban === "0" || sum === "0") {
+      alert("Nie je možné vygenerovať QR kód, pokiaľ nie je zadaný IBAN a suma platby.");
+      return;
+    }
+
+    // Vymazanie starého QR kódu po aktualizovaní formuláru vygenerovaním nového QR kódu
+    $('#previewModal .modal-body img').remove();
+    $('#qrPrompt2').hide();
+    $('#qrPrompt').show();
+    
+    // Vytvorenie URL pre GET request (URL upravená podľa dokumentácii API)
+    var getUrl = 'https://api.freebysquare.sk/pay/v1/generate-png-v2?size=200&color=3&transparent=true&payments=[{"amount":' + sum + ',"currencyCode":"' + moneytype + '","paymentDueDate":"' + <?php echo $paymentDueDate; ?> + '","variableSymbol":"' + vs + '","constantSymbol":"' + ks + '","specificSymbol":"' + ss + '","paymentNote":"' + info_name + '","beneficiaryName":"' + name + '","beneficiaryAddressLine1":"' + adress + '","beneficiaryAddressLine2":"' + adress2 + '","bankAccounts":[{"iban":"' + iban + '","bic":null}]}]';
+
+    // Vytvorenie JSON dát pre POST request podľa dokumentácii API
+    var postData = {
+      "size": 200,
+      "color": 3,
+      "transparent": true,
+      "payments": [
+        {
+          "amount": sum,
+          "currencyCode": moneytype,
+          "paymentDueDate": "<?php echo $paymentDueDate; ?>",
+          "variableSymbol": vs,
+          "constantSymbol": ks,
+          "specificSymbol": ss,
+          "paymentNote": info_name,
+          "beneficiaryName": name,
+          "beneficiaryAddressLine1": adress,
+          "beneficiaryAddressLine2": adress2,
+          "bankAccounts": [
+            {
+              "iban": iban,
+              "bic": null
+            }
+          ]
+        }
+      ]
+    };
+
+    // Vytvorenie novej XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://api.freebysquare.sk/pay/v1/generate-png-v2', true);
+    xhr.responseType = 'blob';
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function(e) {
+      if (this.status == 200) {
+        var blob = this.response;
+        var img = document.createElement('img');
+        img.onload = function(e) {
+          window.URL.revokeObjectURL(img.src);
+        };
+        img.src = window.URL.createObjectURL(blob);
+        img.className = 'mx-auto d-block';
+        $('#previewModal .modal-body').append(img);
+        $('#qrPrompt').hide();
+        $('#qrPrompt2').show();
+      }
+    };
+    xhr.send(JSON.stringify(postData));
+  }
+</script>
 
 <?php include 'inc/footer.php'; ?>
