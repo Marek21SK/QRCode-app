6120c05ca953315e4061dcf69ee8a06931b121d9
Marek Pástor <marekpastorml@gmail.com>
2024-03-03 12:33:26 +0100

    Pridanie moment.js do projektu

--- app/inc/header.php
+++ app/inc/header.php
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.30.1/moment.min.js"></script>
   <title>Zdieľanie platby</title>
 </head>
 <body>
