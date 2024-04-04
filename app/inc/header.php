1a17d8f86a80e36339c93b4d5483df8f1d5d4493
Marek Pástor <99325221+Marek21SK@users.noreply.github.com>
2024-04-04 13:45:46 +0200

    Pridanie DataTables kničnice do projektu

--- app/inc/header.php
+++ app/inc/header.php
@@ -8,9 +8,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdn.datatables.net/v/dt/dt-2.0.3/datatables.min.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.30.1/moment.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="https://cdn.datatables.net/v/dt/dt-2.0.3/datatables.min.js"></script>
   <link rel="stylesheet" type="text/css" href="styles/style.css"> 
   <title>Zdieľanie platby</title>
 </head>
