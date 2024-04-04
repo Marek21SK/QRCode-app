f6884657a11b4053e4c0416db22c232765f8e3f6
Marek Pástor <99325221+Marek21SK@users.noreply.github.com>
2024-04-04 13:48:16 +0200

    Zmeny v tabuľke + DataTables

--- app/index.php
+++ app/index.php
@@ -32,8 +32,8 @@
     </div>
 
     <div class="container" style="margin-bottom: 10px;">
-    <button class="toggleButton btn btn-primary mx-auto d-flex justify-content-center align-items-center" style="height: 25px; font-size: 0.6rem; text-align: left;">Otvoriť prehľad IBAN-ov</button>
-      <div class="row" style="display: none;"><br>
+    <h4 class="d-flex justify-content-center align-items-center">Prehľad IBAN-ov</h4><br>
+      <div class="row"><br>
         <div class="col-md-12">
           <!-- Zobrazenie prehľadu IBAN-ov -->
           <?php
@@ -48,11 +48,8 @@
           ?>
             <div style="width: 100%; max-width: 800px; margin: auto;">
               <div class="table-responsive">
-                <table class="table table-bordered">
+                <table id="ibanTable" class="table table-bordered">
                   <thead>
-                    <tr>
-                      <th colspan="2" class="text-center">Prehľad IBAN-ov</th>
-                    </tr>
                     <tr>
                       <th scope="col" style="width: 40%;">IBAN</th>
                       <th scope="col" style="width: 60%;">Názov</th>
@@ -72,62 +69,8 @@
             </div>
           <?php
           } else {
-            echo "<div class='mx-auto d-flex justify-content-center align-items-center'>Žiadne IBAN pre zobrazenie.</div>";
-          }
-          ?>
-        </div>
-      </div>
-
-      <!-- Formulár na pridanie IBAN -->
-      <div class="row mt-3">
-        <div class="col-md-6 mx-auto d-flex justify-content-center align-items-center"><br>
-          <div class="card" style="padding: 10px; margin-bottom: 10px; border-radius: 15px; border-width: medium; border-color: lightgrey;">
-            <div class="card-body">
-              <form action="config/add_iban_process.php" method="POST">
-                <div class="center-container" style="display: flex; justify-content: center;">
-                  <div class="row">
-                    <div class="mb-3">
-                      <label for="add_iban" class="form-label">Zadajte IBAN:</label>
-                      <input style="padding-right: 25px; margin: 10px auto;" type="text" class="form-control" id="add_iban" name="iban" placeholder="SK88 8888 8888 8888 8888 8888" maxlength="29" required>
-                      <label for="iban_name" class="form-label">Popis IBAN-u:</label>
-                      <textarea style="padding-right: 25px; margin: 10px auto;" type="text" class="form-control" id="iban_name" name="iban_name" placeholder="Prosím, uveďte účel použitia tohto IBAN" rows="3" maxlength="255" required></textarea>
-                      <div class="d-grid">
-                        <?php if (isset($_SESSION['error4'])) {
-                          echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error4'] . '</div>';
-                          unset($_SESSION['error4']);
-                        }
-                        if (isset($_SESSION['error5'])) {
-                          echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error5'] . '</div>';
-                          unset($_SESSION['error5']);
-                        }
-                        if (isset($_SESSION['success3'])) {
-                          echo '<div class="alert alert-info" role="alert">' . $_SESSION['success3'] . '</div>';
-                          unset($_SESSION['success3']);
-                        }
-                        if (isset($_SESSION['error6'])) {
-                          echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error6'] . '</div>';
-                          unset($_SESSION['error6']);
-                        }
-                        if (isset($_SESSION['error7'])) {
-                          echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error7'] . '</div>';
-                          unset($_SESSION['error7']);
-                        }
-                        if (isset($_SESSION['error8'])) {
-                          echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error8'] . '</div>';
-                          unset($_SESSION['error8']);
-                        }
-                        if (isset($_SESSION['error9'])) {
-                          echo '<div class="alert alert-danger" role="alert">' . $_SESSION['error9'] . '</div>';
-                          unset($_SESSION['error9']);
-                        } ?>
-                      </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Pridať IBAN</button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
+            echo "<div class='mx-auto d-flex justify-content-center align-items-center'>
+                    <h6>Žiadne IBAN pre zobrazenie.</h6></div>";}?>
         </div>
       </div>
     </div>
@@ -154,28 +97,30 @@ function formatIBAN($iban)
 }
 ?>
 
-<!-- Skript, ktorý pri pridávaní IBAN-u sa zobrazí tak, že každé 4 znaky dá medzeru pre lepšie skontrolovanie -->
+<!-- Skript pre tabuľku DataTables -->
 <script>
-  document.getElementById('add_iban').addEventListener('input', function (event) {
-    var ibanSpaces = event.target.value.replace(/\s/g, '');
-    var ibanFormat = ibanSpaces.replace(/(.{4})/g, '$1 ').trim();
-    event.target.value = ibanFormat;
-  });
-</script>
-
-<!-- Skript na zobrazenie/skrytie IBAN-ov -->
-<script>
-  document.querySelectorAll(".toggleButton").forEach(function(button) {
-    button.addEventListener("click", function() {
-      var content = this.nextElementSibling;
-      if (content.style.display === "none") {
-        content.style.display = "block";
-        this.textContent = "Zatvoriť prehľad IBAN-ov";
-      } else {
-        content.style.display = "none";
-        this.textContent = "Otvoriť prehľad IBAN-ov";
-      }
-    });
+  $(document).ready(function() {
+      $('#ibanTable').DataTable({
+          "pageLength": 10,
+          "lengthMenu": [[5, 10, 15, 20], [5, 10, 15, "MAX"]],
+          "searching": true,
+          "language": {
+              "lengthMenu": "Zobraziť _MENU_ záznamov na stránku",
+              "zeroRecords": "Nič sa nenašlo - ospravedlňujeme sa",
+              "info": "Zobrazenie strany _PAGE_ z _PAGES_",
+              "infoEmpty": "Žiadne záznamy k dispozícii",
+              "infoFiltered": "(filtrované z celkových _MAX_ záznamov)",
+              "search": "Hľadať:",
+              "paginate": {
+                  "next":       "Ďalší",
+                  "previous":   "Predchádzajúci"
+              }
+          },
+          "order": [[2, 'asc']],
+          "initComplete": function(settings, json){
+            $('#ibanTable').show();
+          }
+      });
   });
 </script>
 
