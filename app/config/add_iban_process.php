c75cf302888941dfcefe05467752814020806bb0
Marek Pástor <99325221+Marek21SK@users.noreply.github.com>
2024-04-04 13:46:20 +0200

    Úpravy v HTML headery

--- app/config/add_iban_process.php
+++ app/config/add_iban_process.php
@@ -50,28 +50,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Kontrola, IBAN začíná prefixom "SK"
     if (substr($iban, 0, 2) !== 'SK') {
         $_SESSION['error5'] = "IBAN musí začínať prefixom 'SK'.";
-        header("Location: /qrcode-app/app/index.php");
+        header("Location: /qrcode-app/app/ibans.php");
         exit();
     }
 
     // Za prefixom "SK" len číslice
     if (!preg_match('/^SK\d+$/', substr($iban, 0, 4))) {
         $_SESSION['error6'] = "IBAN po prefixe 'SK' môže obsahovať len číslice.";
-        header("Location: /qrcode-app/app/index.php");
+        header("Location: /qrcode-app/app/ibans.php");
         exit();
     }
 
     // Kontrola, či IBAN má správny formát (prefix "SK" a 22 čísiel)
     if (!preg_match('/^SK\d{22}$/', $iban)) {
         $_SESSION['error7'] = "IBAN musí mať správny formát.";
-        header("Location: /qrcode-app/app/index.php");
+        header("Location: /qrcode-app/app/ibans.php");
         exit();
     }
 
     // Kontrola, či je IBAN platný (Presun prvých 4 znakov na koniec a kontrola či je číslo deliteľné 97)
     if (!isIBANValid($iban)) {
         $_SESSION['error9'] = "IBAN nie je platný.";
-        header("Location: /qrcode-app/app/index.php");
+        header("Location: /qrcode-app/app/ibans.php");
         exit();
     }
 
@@ -84,7 +84,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($checkIBANresult-> num_rows > 0){
         $_SESSION['error8'] = "Nemôžete si pridať už raz pridaný IBAN, použite iný.";
-        header("Location: /qrcode-app/app/index.php");
+        header("Location: /qrcode-app/app/ibans.php");
         exit();
     }
 
