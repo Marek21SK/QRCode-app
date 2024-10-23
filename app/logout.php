42d13d0108c3194ef590bdba2b6fb36917a94b15
Marek Pástor <99325221+Marek21SK@users.noreply.github.com>
2024-10-23 15:25:44 +0200

    Odstránenie všetkých súborov z repozitára

--- app/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'config/common.php';
-// Po odhlásení používateľa zničí session daného užívateľa a redirektne na login.php
-session_destroy();
-header("Location: ../index.php");
-exit();
-?>
